bb8657c7c2833fffb072ea9c57a8980f19bfe4ca
Marcio Mendes <mmendes.as@gmail.com>
2019-11-10 12:41:48 -0300

    feat: loading repository page

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,54 @@
-import React from 'react';
+/* eslint-disable react/static-property-placement */
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+import { Loading } from './styles';
+
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando...</Loading>;
+    }
+    return <h1>Repository:</h1>;
+  }
 }
