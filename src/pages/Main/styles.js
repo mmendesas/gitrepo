d94379f190771dc79e58f912d997594fe210b965
Marcio Mendes <mmendes.as@gmail.com>
2019-11-10 02:34:06 -0300

    chore: add global styles with styled-components

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 24px;
+  color: ${props => (props.error ? 'red' : '#fff')};
+  font-family: Arial, Helvetica, sans-serif;
+
+  small {
+    padding-left: 15px;
+    color: #000;
+  }
+`;
